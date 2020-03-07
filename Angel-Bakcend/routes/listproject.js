1b4a13299b52241bce5ca6addf129401068c1dc1
rafael boschllado <rbllado@outlook.com>
2020-03-07 15:55:12 +0100

    showing the project already in front

--- Angel-Bakcend/routes/listproject.js
+++ Angel-Bakcend/routes/listproject.js
@@ -8,34 +8,29 @@ const User = require("../models/User");
 const Projects = require("../models/Projects");
 
 // when arrives here localhost:5000/listproject
-router.get("/",(req, res, next) => {
+router.get("/", (req, res, next) => {
 
   const userId = req.session.currentUser._id;
 
+  //project that the user have.
   User.findById(userId)
-
-    .populate("Project")
     .then(project => {
-      console.log(project.project[0]);
-      res.json(project.project[0])
-      
+      //in that case it is only the first one
+      let projectId = project.project[0];
+
+      //returning the description of project
+      Projects.findById(projectId)
+        .then(detailProject => {
+          console.log(detailProject);
+          res.status(200).json((detailProject));
+        })
+        .catch(err => console.log(err))
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
+
 });
 
 module.exports = router;
 
 
 
-
-//only the first project
-
-// let projectId = project.project[0];
-      
-// //returning the value of project
-// Projects.findById(projectId)
-//   .then(detailProject => {
-//     console.log(detailProject);
-//     res.status(200).json((detailProject));
-//   })
-//   .catch(err => console.log(err))
