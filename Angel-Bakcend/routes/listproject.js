2dffcf58a84cc29e826d7f0c6ef4a53b284b0976
rafael boschllado <rbllado@outlook.com>
2020-03-07 15:06:41 +0100

    missing json data to react

--- Angel-Bakcend/routes/listproject.js
+++ Angel-Bakcend/routes/listproject.js
@@ -8,28 +8,34 @@ const User = require("../models/User");
 const Projects = require("../models/Projects");
 
 // when arrives here localhost:5000/listproject
-router.get("/", async (req, res, next) => {
+router.get("/",(req, res, next) => {
 
   const userId = req.session.currentUser._id;
 
-  await User.findById(userId)
+  User.findById(userId)
 
-    .populate("projects")
+    .populate("Project")
     .then(project => {
-      //only the first project
-
-      let projectId = project.project[0];
-
-      //returning the value of project
-      Projects.findById(projectId)
-        .then(detailProject => {
-          console.log((detailProject));
-          res.json(detailProject);
-        })
-        .catch(err => console.log(err))
-      //res.json(project);
+      console.log(project.project[0]);
+      res.json(project.project[0])
+      
     })
     .catch(err => console.log(err));
 });
 
 module.exports = router;
+
+
+
+
+//only the first project
+
+// let projectId = project.project[0];
+      
+// //returning the value of project
+// Projects.findById(projectId)
+//   .then(detailProject => {
+//     console.log(detailProject);
+//     res.status(200).json((detailProject));
+//   })
+//   .catch(err => console.log(err))
